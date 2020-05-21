64d7497a61c77effa933f06a16fb4ee028dc6728
Daman Bhola <42509298+damnbhola@users.noreply.github.com>
2020-05-21 23:35:11 +0530

    Sorting

--- Sorting/insertionSort.py
+++ Sorting/insertionSort.py
@@ -1,8 +1,8 @@
 """
-Bubble Sort
+Insertion Sort
 
 Write a function that takes in an array of integers and returns a sorted version of that array.
-Use the Bubble Sort algorithm to sort the array.
+Use the Insertion Sort algorithm to sort the array.
 
 Sample Input: [8, 5, 2, 9, 5, 6, 3]
 Sample Output: [2, 3, 5, 5, 6, 8, 9]
@@ -12,16 +12,12 @@ Sample Output: [2, 3, 5, 5, 6, 8, 9]
 # Best: O(n) time | O(1) space
 # Average: O(n^2) time | O(1) space
 # Worst: O(n^2) time | O(1) space
-def bubbleSort(array):
-    isSorted = False
-    counter = 0
-    while not isSorted:
-        isSorted = True
-        for i in range(len(array) - 1 - counter):
-            if array[i] > array[i + 1]:
-                swap(i, i + 1, array)
-                isSorted = False
-        counter += 1
+def insertionSort(array):
+    for i in range(1, len(array)):
+        j = i
+        while j > 0 and array[j] < array[j - 1]:
+            swap(j, j - 1, array)
+            j -= 1
     return array
 
 
